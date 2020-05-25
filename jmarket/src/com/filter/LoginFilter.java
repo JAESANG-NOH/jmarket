85b4bafa6441a7e86c06ed4084412ebad7141067
sist <sist@DESKTOP-RFG95MQ>
2020-05-25 19:05:10 +0900

    승현

---
+++ jmarket/src/com/filter/LoginFilter.java
@@ -0,0 +1,83 @@
+//package com.filter;
+//
+//import java.io.IOException;
+//
+//import javax.servlet.Filter;
+//import javax.servlet.FilterChain;
+//import javax.servlet.FilterConfig;
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
+//
+//import com.user.SessionInfo;
+//
+//@WebFilter("/*")
+//public class LoginFilter implements Filter{
+//
+//	@Override
+//	public void init(FilterConfig filterConfig) throws ServletException {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//			throws IOException, ServletException {
+//		HttpServletRequest req=(HttpServletRequest)request;
+//		HttpSession session=req.getSession();
+//		SessionInfo info=(SessionInfo) session.getAttribute("member");
+//		
+//		if(info==null&&isExcludeUri(req)==false) {
+//			String path="/WEB-INF/views/member/login.jsp";
+//			RequestDispatcher rd=req.getRequestDispatcher(path);
+//			rd.forward(request, response);
+//			return;
+//		}
+//		
+//		chain.doFilter(request, response);
+//		
+//		
+//	}
+//
+//	@Override
+//	public void destroy() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
+//	private boolean isExcludeUri(HttpServletRequest req) {
+//		String uri=req.getRequestURI();
+//		String cp=req.getContextPath();
+//		uri=uri.substring(cp.length());
+//		
+//		String []uris= {  //로그인 필요하지 않은것들
+//				"/index.jsp", "/main.do", "/member/login.do",
+//				"/member/login_ok.do", "/member/member.do",
+//				"/member/member_ok.do", "/member/userIdCheck.do",
+//				"/notice/list.do",
+//				"/resource/**"  //**:밑에 있는것 다 허용
+//		};
+//		
+//		if(uri.length()<=1)
+//			return true;
+//		
+//		for(String s:uris) {
+//			if(s.lastIndexOf("/**")!=-1) {
+//				s=s.substring(0,s.lastIndexOf("**"));
+//				if(uri.indexOf(s)==0) {
+//					return true;
+//				}
+//				
+//			}else if(uri.equals(s)) {
+//				return true;
+//			}
+//		}
+//		
+//		return false;
+//	}
+//	
+//}
